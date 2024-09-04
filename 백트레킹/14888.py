3b8c6826d435c28437a22b2ac8d8b53ad3e3d6b4
hbs5186 <hbs5186@naver.com>
2024-09-04 18:55:39 +0900

    2024-09-04

--- 백트레킹/14888.py
+++ 백트레킹/14888.py
@@ -1,27 +1,27 @@
-import sys
+from itertools import permutations
 
-n=int(sys.stdin.readline())
-num=list(map(int,sys.stdin.readline().split()))
-arr=list(map(int,sys.stdin.readline().split()))
-arr1=[]
-for i in range(4):
-    if arr[i]:
-        for _ in range(arr[i]):
-            if i==0:
-                arr1.append('+')
-            if i==1:
-                arr1.append('-')
-            if i==2:
-                arr1.append('*')
-            if i==3:
-                arr1.append('//')
-print(arr1)
-max=0
-min=0
-answer=''
+n = int(input())
+number = list(map(int, input().split()))
+add, sub, mul, div = map(int, input().split());
+max_result = - int(1e9)
+min_result = int(1e9)
 
-def back(i):
-    result=0
-    if i==n:
-        return result
+def dfs(add, sub, mul, div, sum, idx):
+    global max_result, min_result
+    if idx == n:
+        max_result = max(max_result, sum)
+        min_result = min(min_result, sum)
+        return
+    if add:
+        dfs(add - 1, sub, mul, div, sum + number[idx], idx + 1)
+    if sub:
+        dfs(add, sub - 1, mul, div, sum - number[idx], idx + 1)
+    if mul:
+        dfs(add, sub, mul - 1, div, sum * number[idx], idx + 1)
+    if div:
+        dfs(add, sub, mul, div - 1, int(sum / number[idx]), idx + 1)
 
+
+dfs(add, sub, mul, div, number[0], 1)
+print(max_result)
+print(min_result)
