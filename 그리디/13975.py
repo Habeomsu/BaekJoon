4560154370805f12be2333b52fbcdbbe7d83ad29
hbs5186 <hbs5186@naver.com>
2024-09-05 14:53:54 +0900

    2024-09-04

---
+++ 그리디/13975.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+n = int(sys.stdin.readline())
+for i in range(n):
+    m= int(sys.stdin.readline())
+    arr=list(map(int,sys.stdin.readline().split()))
+    heapq.heapify(arr)
+    result=0
+    while len(arr)!=1:
+        a=heapq.heappop(arr)
+        b=heapq.heappop(arr)
+        result +=(a+b)
+        heapq.heappush(arr,a+b)
+    print(result)
